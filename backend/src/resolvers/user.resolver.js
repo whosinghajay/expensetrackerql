68d21f64cb379cb5e25a50562d2d184690661903
Ajay Singh <whosinghajay@gmail.com>
2024-12-14 08:14:04 +0530

    resolvers created

---
+++ backend/src/resolvers/user.resolver.js
@@ -0,0 +1,16 @@
+import { users } from "../utils/dummyData.js";
+
+const userResolver = {
+  Query: {
+    users: () => {
+      return users;
+    },
+    user: (_, { userId }) => {
+      return users.find((user) => user._id === userId);
+    },
+  },
+
+  Mutation: {},
+};
+
+export default userResolver;
