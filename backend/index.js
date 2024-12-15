fb986bc31d1d9fd228f48796c7e86c90ce380fa6
Ajay Singh <whosinghajay@gmail.com>
2024-12-15 13:48:06 +0530

    server endpoint changed from / to /graphql

--- backend/index.js
+++ backend/index.js
@@ -68,7 +68,7 @@ await server.start();
 // Set up our Express middleware to handle CORS, body parsing,
 // and our expressMiddleware function.
 app.use(
-  "/",
+  "/graphql",
   cors({
     origin: "http://localhost:3000",
     credentials: true,
@@ -85,7 +85,7 @@ app.use(
 // Modified server startup
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
 await connectDB();
-console.log(`🚀 Server ready at http://localhost:4000/`);
+console.log(`🚀 Server ready at http://localhost:4000/graphql`);
 
 //-----------------------------------------------------------
 
