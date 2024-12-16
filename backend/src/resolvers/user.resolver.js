9c1bad372909d8ec521e5484c33cc94e8faa70ec
Ajay Singh <whosinghajay@gmail.com>
2024-12-16 13:49:04 +0530

    user resolver req modified

--- backend/src/resolvers/user.resolver.js
+++ backend/src/resolvers/user.resolver.js
@@ -55,10 +55,10 @@ const userResolver = {
     logout: async (_, __, context) => {
       try {
         await context.logout();
-        req.session.destroy((error) => {
+        context.req.session.destroy((error) => {
           if (error) throw error;
         });
-        res.clearCookie("connect.sid");
+        context.res.clearCookie("connect.sid");
 
         return { message: "Logged out successfully!" };
       } catch (error) {
