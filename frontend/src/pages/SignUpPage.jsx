7438ab7805f51863015c4f173b7a47f1b65a46bd
Ajay Singh <whosinghajay@gmail.com>
2024-12-16 13:50:19 +0530

    signup page functionality done

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
+
 import InputField from "../components/InputField.jsx";
 import RadioButton from "../components/RadioButton.jsx";
+
+import { useMutation } from "@apollo/client";
+import { SIGN_UP } from "../graphql/mutations/user.mutation.js";
 
 const SignUpPage = () => {
   const [signUpData, setSignUpData] = useState({
@@ -10,6 +15,24 @@ const SignUpPage = () => {
     password: "",
     gender: "",
   });
+
+  const [signup, { loading, error }] = useMutation(SIGN_UP, {
+    refetchQueries: ["GetAuthenticatedUser"],
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signup({
+        variables: {
+          input: signUpData,
+        },
+      });
+    } catch (error) {
+      console.error("Error: ", error);
+      toast.error(error.message);
+    }
+  };
 
   const handleChange = (e) => {
     const { name, value, type } = e.target;
@@ -25,11 +48,6 @@ const SignUpPage = () => {
         [name]: value,
       }));
     }
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log(signUpData);
   };
 
   return (
@@ -90,8 +108,9 @@ const SignUpPage = () => {
                 <button
                   type="submit"
                   className="w-full bg-black text-white p-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-black  focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300 disabled:opacity-50 disabled:cursor-not-allowed"
+                  disabled={loading}
                 >
-                  Sign Up
+                  {loading ? "Loading..." : "Sign Up"}
                 </button>
               </div>
             </form>
