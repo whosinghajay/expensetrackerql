319a9df980170bedf02f3b5dcebd497ccdde64bf
Ajay Singh <whosinghajay@gmail.com>
2024-12-16 13:52:08 +0530

    apollo client config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,26 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App.jsx";
 import GridBackground from "./components/ui/GridBackground.jsx";
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql", // URL of of our graphql server
+  cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them.
+  credentials: "include", // This tells Apollo Client to send cookies along with every request to the server.
+});
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <GridBackground>
-        <App />
+        <ApolloProvider client={client}>
+          <App />
+        </ApolloProvider>
       </GridBackground>
     </BrowserRouter>
   </StrictMode>
