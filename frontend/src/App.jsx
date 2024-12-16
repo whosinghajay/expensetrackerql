fed0b41c64a24260e4833d6c8be5d0f50e8f1d68
Ajay Singh <whosinghajay@gmail.com>
2024-12-16 13:51:31 +0530

    modification done based on auth user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 import HomePage from "./pages/HomePage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
@@ -7,18 +8,37 @@ import TransactionPage from "./pages/TransactionPage.jsx";
 import NotFoundPage from "./pages/NotFoundPage.jsx";
 import Header from "./components/ui/Header.jsx";
 
+import { useQuery } from "@apollo/client";
+import { GET_AUTHENTICATED_USER } from "./graphql/queries/user.query.js";
+
 function App() {
-  const authUser = true;
+  const { loading, data, error } = useQuery(GET_AUTHENTICATED_USER);
+
   return (
     <>
-      {authUser && <Header />}
+      {data?.authUser && <Header />}
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/transaction/:id" element={<TransactionPage />} />
+        <Route
+          path="/"
+          element={data?.authUser ? <HomePage /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/login"
+          element={!data?.authUser ? <LoginPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/signup"
+          element={!data?.authUser ? <SignUpPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/transaction/:id"
+          element={
+            data?.authUser ? <TransactionPage /> : <Navigate to={"/login"} />
+          }
+        />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
+      <Toaster />
     </>
   );
 }
