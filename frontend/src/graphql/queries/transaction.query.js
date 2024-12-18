daa5facb8f1ec6dbb036d9bf03c7a36afdc35578
Ajay Singh <whosinghajay@gmail.com>
2024-12-18 07:39:48 +0530

    get transaction statistics

--- frontend/src/graphql/queries/transaction.query.js
+++ frontend/src/graphql/queries/transaction.query.js
@@ -27,3 +27,13 @@ export const GET_TRANSACTION = gql`
     }
   }
 `;
+
+
+export const GET_TRANSACTION_STATISTICS = gql`
+	query GetTransactionStatistics {
+		categoryStatistics {
+			category
+			totalAmount
+		}
+	}
+`;
