febc40f666f9d3f852af83aef1851f6f70eefb7a
Ajay Singh <whosinghajay@gmail.com>
2024-12-18 08:37:13 +0530

    graphql relationship understanding and creation of getuserandtransactions

--- frontend/src/graphql/queries/user.query.js
+++ frontend/src/graphql/queries/user.query.js
@@ -10,3 +10,25 @@ export const GET_AUTHENTICATED_USER = gql`
     }
   }
 `;
+
+export const GET_USER_AND_TRANSACTIONS = gql`
+  query GetUserAndTransactions($userId: ID!) {
+    user(userId: $userId){
+      _id 
+      name
+      username
+      profilePicture
+      #relationship
+      transactions{
+        _id
+        description
+        paymentType
+        category
+        amount
+        location
+        date
+      }
+    }
+  }
+`;
+
