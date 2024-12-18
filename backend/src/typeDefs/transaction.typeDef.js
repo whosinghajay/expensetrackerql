c5391cca1e76e0cdbb900cc22fd7b5bffedd171d
Ajay Singh <whosinghajay@gmail.com>
2024-12-18 07:41:32 +0530

    categoryStatistics resolver added

--- backend/src/typeDefs/transaction.typeDef.js
+++ backend/src/typeDefs/transaction.typeDef.js
@@ -13,6 +13,7 @@ const transactionTypeDef = `#graphql
     type Query {
         transactions: [Transaction!]
         transaction(transactionId: ID!): Transaction
+        categoryStatistics: [CategoryStatistics!]
     }
 
     type Mutation {
@@ -20,6 +21,11 @@ const transactionTypeDef = `#graphql
         updateTransaction(input: UpdateTransactionInput!): Transaction!
         deleteTransaction(transactionId: ID!): Transaction!
     }
+
+    type CategoryStatistics {
+    category: String!
+    totalAmount: Float!
+  }
 
     input CreateTransactionInput {
         description: String!
